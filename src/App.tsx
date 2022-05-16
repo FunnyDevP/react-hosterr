055fa19185cde47a66431349c4f58bccc2c7aefb
Preaw <waranchit.ch@gmail.com>
2022-05-16 22:54:21 +0700

    set main container to direction is column

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.scss";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 const App = (): JSX.Element => (
   <div className="main">
     <Header />
+    <div style={{ border: "1px solid" }}>2</div>
+    <Footer />
   </div>
 );
 
