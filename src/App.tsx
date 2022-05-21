df53bbf9be89c80d491759470eb9423ece61fa7a
Preaw <waranchit.ch@gmail.com>
2022-05-21 18:33:25 +0700

    use content in main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.scss";
 import Header from "./components/header/Header";
+import Content from "./components/content/Content";
 import Footer from "./components/footer/Footer";
 
 const App = (): JSX.Element => (
   <div className="main">
     <Header />
-    <div style={{ border: "1px solid" }}>2</div>
+    <div>
+      <Content />
+    </div>
     <Footer />
   </div>
 );
